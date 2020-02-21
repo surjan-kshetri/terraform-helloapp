3f52f8d612f8b94c15d7df419522a82faef0539b
surjan-kshetri <kshe9881@stthomas.edu>
2020-02-20 20:14:24 -0500

    Added

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_files = "/home/intelycorellc/.aws/credentials"
+  shared_credentials_file = "/home/intelycorellc/.aws/credentials"
   profile = "surjan"
 }

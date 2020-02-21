3e3173955d24cd4acd9d016e1dedfee8ea0cf8a6
surjan-kshetri <kshe9881@stthomas.edu>
2020-02-20 19:59:33 -0500

    Added files again

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_launch_configuration" "default" {
-  name          = "terraform-sprinboot-helloapp"
+  name          = "terraform-springboot-helloapp"
   image_id      = "ami-04ff587bc1c1d65e6"
   instance_type = "t2.micro"
   key_name = "terraform-springboot-helloapp"

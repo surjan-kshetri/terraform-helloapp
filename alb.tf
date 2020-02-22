d469bb239d99c6ec60748587d69c3b5faba851f3
surjan-kshetri <kshe9881@stthomas.edu>
2020-02-21 19:21:59 -0500

    Added lb

--- alb.tf
+++ alb.tf
@@ -11,3 +11,20 @@ resource "aws_lb" "default" {
     Environment = "dev"
   }
  }
+
+resource "aws_lb_listener" "default" {
+  load_balancer_arn = "${aws_lb.default.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.default.arn}"
+  }
+}
+
+resource "aws_lb_target_group" "default" {
+  name     = "terraform-springboot-helloapp"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = "vpc-d86355a2"
+}
